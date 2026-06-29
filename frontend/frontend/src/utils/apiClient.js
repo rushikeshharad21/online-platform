6aa1cd4eb20df7c4d097b5eb4fc7bc35716db4dd
rushikeshharad21 <rushikeshharad21@gmail.com>
2026-06-29 16:29:37 +0530

    new changes

--- frontend/frontend/src/utils/apiClient.js
+++ frontend/frontend/src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL +"/api" || 'http://localhost:5000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
