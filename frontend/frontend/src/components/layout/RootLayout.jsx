a9ee75cb1d8f14c8e3238f8c8d6db7d05c6a8f90
rushikeshharad21 <rushikeshharad21@gmail.com>
2026-07-01 20:56:19 +0530

    new

--- frontend/frontend/src/components/layout/RootLayout.jsx
+++ frontend/frontend/src/components/layout/RootLayout.jsx
@@ -25,7 +25,7 @@ const theme = createTheme({
   },
 });
 
-const BG_IMAGE = 'https://images.unsplash.com/photo-1522441815192-d9f04eb0615c?w=500&auto=format&fit=crop&q=90&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D';
+const BG_IMAGE = 'https://plus.unsplash.com/premium_photo-1701892428860-ca4913e92274?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmFja2dyb3VuZCUyMHVpJTIwaW1hZ2VzfGVufDB8fDB8fHww';
 
 const tk = {
   navy:       'rgba(5,10,30,0.55)',
